762eba0f54fa16273c05d7b15ad6ad62abb40530
Emily <emily.a.harber@gmail.com>
2020-02-05 15:55:38 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Add your functions here
+
+function map(array,func){
+  let r = []
+  array.forEach(elem => {
+    r.push( func(elem) )
+  })
+  return r
+}
+
+function reduce(array, func, start = false){
+  array.forEach(elem => {
+    start = func(elem, start)
+  })
+  return start
+}
